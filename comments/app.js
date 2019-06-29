3ac504aab963788220b7b94151016df44982f2ab
yusong <1193950650@qq.com>
2019-06-30 02:20:32 +0800

    react init product

---
+++ comments/app.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+
+class app extends Component {
+    render() {
+        return (
+            <div>THIS IS NEW React app</div>
+        );
+    }
+}
+
+export default app
---
+++ src/app1.js
@@ -0,0 +1,11 @@
+import React ,{Component} from 'react'
+
+class Newapp extends Component{
+    render(){
+        return (
+            <div>aaaaaaa</div>
+        )
+    }
+}
+
+export default Newapp
